c95d86b3a3c09e38a8ad98b8c8d98d7041feeaff
carnielJoao <joaovictorcarniel.cert.trabalho@gmail.com>
2024-11-08 14:51:25 -0300

    Enviando os arquivos do projeto

---
+++ config/db.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
+
+// console.log(dbUser)
+// console.log(dbPassword)
+
+const conn = async () => {
+    try {
+        const dbConn = await mongoose.connect(
+            `mongodb+srv://${dbUser}:${dbPassword}@cluster0.wevgi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+        );
+        
+
+
+        console.log("Conectou ao banco!")
+
+        return dbConn
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+conn()
+
+module.exports = conn
+
